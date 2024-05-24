72e4fa7df5702c9c8b71734956b2e3356204693b
Chris Miles <akkadius1@gmail.com>
2024-05-23 21:49:26 -0500

    Update entrypoint.pl

--- containers/eqemu-server/entrypoint.pl
+++ containers/eqemu-server/entrypoint.pl
@@ -58,6 +58,7 @@ print `while true; do nohup ~/assets/scripts/process-watcher.pl && break; done >
 print "# chmod/chown | scripts\n";
 print `sudo chmod +x ~/assets/scripts/*`;
 print `sudo chown eqemu -R ~/.ccache`;
+print `sudo chown eqemu -R ~/.cache`;
 
 #########################
 # bash symlinks
